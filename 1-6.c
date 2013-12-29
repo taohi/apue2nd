b3f6bf2ef07d544dce0b23f7c61b4001e8f8305c
whatcanyou <501256595@qq.com>
2013-12-29 13:59:07 +0000

    modify 1-6.c

--- 1-6.c
+++ 1-6.c
@@ -3,7 +3,8 @@
 int main(int argc,char *argv[])
 {
     fprintf(stderr,"EACCES:%s\n",strerror(EACCES));
-    errno = ENOENT;
+  //  errno = ENOENT;
+    errno = ENOMEM;
     perror(argv[0]);
     exit(0);
 }
